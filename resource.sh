f6defc0750f95cc2bfded1330d16524faf264a03
suraj <surajjosh7@gmail.com>
2024-01-04 20:01:11 +0530

    updated

--- resource.sh
+++ resource.sh
@@ -2,10 +2,14 @@
 
 cpu_info=$(top -bn1 | grep '^%Cpu')
 id_value=$(echo "$cpu_info" | cut -d',' -f4 | cut -d' ' -f2)
-echo "$id_value"
+float=$(echo "$id_value" | bc -l)
+cpu_percentage=$(echo "100.0 - $float" | bc)
 
-cpu_percentage=$($cpu:34:37)
-    memory=$(free | awk '/Mem/{printf "%.2f\n", $3/$2 * 100}')
-    echo "{\"cpu\": \"$id_value%\", \"memory\": \"$memory%\"}"
+
+
+memory=$(free -tm | grep 'Total' | awk '{print($4)}')
+total_memory=$(free -tm | grep 'Total' | awk '{print($2)}')
+echo "{\"cpu\": $cpu_percentage, \"free_Ram\": $memory,\"Total_Ram\": $total_memory}"
+
+
    
-
